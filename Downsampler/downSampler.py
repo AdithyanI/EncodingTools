143d032164bc7c5d92f3844f3ac8cd4991b018b2
Adithyan <adithyan.i4internet@gmail.com>
2017-12-25 17:32:20 +0100

    Downsample a single high resolution to obtain other lower resolution figures.

--- Downsampler/downSampler.py
+++ Downsampler/downSampler.py
@@ -1,8 +1,6 @@
 import subprocess
 import os
 
-struct
-
 def find_highres_source_file(subDirectoryFull):
    # https://stackoverflow.com/questions/3964681/find-all-files-in-a-directory-with-extension-txt-in-python     
     for file in os.listdir(subDirectoryFull):
@@ -11,24 +9,29 @@ def find_highres_source_file(subDirectoryFull):
 
 def down_sampler():
   #https://stackoverflow.com/questions/973473/getting-a-list-of-all-subdirectories-in-the-current-directory
-  cwd = os.getcwd()
+  #cwd = os.getcwd()
+  cwd = "/home/adithyan/Innovation/TestContent"
   subDirectories = next(os.walk(cwd))[1]
-
-  downSampleResolutions = [[640,360],
-                                                  [1280,720]]
+  downSampleResolutions = [
+                                                  [640, 360],
+                                                  [1280, 720]
+                                                ]
   
   for subDirectory in subDirectories:
     subDirectoryFull = cwd + "/" + subDirectory
-
     sourceHighResFile = find_highres_source_file(subDirectoryFull)
-    print(os.path.join(subDirectoryFull, sourceHighResFile))
-    outputLowResFile = sourceHighResFile.replace("1080p", "360p")
 
-	 bashCommand = "ffmpeg -i " + sourceHighResFile " -vf scale=640:360 " + outputLowResFile
-
-
-  #bashCommand = "ffmpeg -i blue_sky_1080p25.y4m -vf scale=640:360 blue_sky_360p25.y4m"
-	#subProcess.Popen(bashCommand)
+    for downSampleResolution in downSampleResolutions:
+      outputLowResFile = sourceHighResFile.replace("1080p", \
+                                      str(downSampleResolution[1])+"p")
+      #print(os.path.join(subDirectoryFull, outputLowResFile))
+      #print(os.path.join(subDirectoryFull, sourceHighResFile))
+      bashCommand = "ffmpeg -i " + sourceHighResFile + " -vf scale=" \
+                                      + str(downSampleResolution[0]) + ":" \
+                                      + str(downSampleResolution[1])  + " " \
+                                      + outputLowResFile
+      p1 = subprocess.Popen(bashCommand, shell=True, cwd=subDirectoryFull)
+      p1.wait()
 
 if __name__ == "__main__":
 	down_sampler()
