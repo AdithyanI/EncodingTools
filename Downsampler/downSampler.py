8defde8f252a8471e9da4781a7d16f20ca173871
Adithyan <adithyan.i4internet@gmail.com>
2017-12-25 18:59:46 +0100

    Pick the correct source file. If the content is already present, do not ovewrite it.

--- Downsampler/downSampler.py
+++ Downsampler/downSampler.py
@@ -3,30 +3,39 @@ import os
 
 def find_highres_source_file(subDirectoryFull):
    # https://stackoverflow.com/questions/3964681/find-all-files-in-a-directory-with-extension-txt-in-python     
+    print(subDirectoryFull)
     for file in os.listdir(subDirectoryFull):
-      if file.endswith(".y4m"):
+      if file.endswith(".y4m") and file.find("1080p")!=-1:
+        print("Found source file = " + file)
         return file
 
 def down_sampler():
   #https://stackoverflow.com/questions/973473/getting-a-list-of-all-subdirectories-in-the-current-directory
   #cwd = os.getcwd()
-  cwd = "/home/adithyan/Innovation/TestContent"
+  cwd = "/home/adithyan/Innovation/RawVideo"
   subDirectories = next(os.walk(cwd))[1]
+  print(subDirectories)
   downSampleResolutions = [
+                                                  [320, 180],
                                                   [640, 360],
                                                   [1280, 720]
                                                 ]
   
   for subDirectory in subDirectories:
     subDirectoryFull = cwd + "/" + subDirectory
-    sourceHighResFile = find_highres_source_file(subDirectoryFull)
+    inputHighResFile = find_highres_source_file(subDirectoryFull)
 
     for downSampleResolution in downSampleResolutions:
-      outputLowResFile = sourceHighResFile.replace("1080p", \
+      outputLowResFile = inputHighResFile.replace("1080p", \
                                       str(downSampleResolution[1])+"p")
-      #print(os.path.join(subDirectoryFull, outputLowResFile))
-      #print(os.path.join(subDirectoryFull, sourceHighResFile))
-      bashCommand = "ffmpeg -i " + sourceHighResFile + " -vf scale=" \
+
+      if os.path.exists(os.path.join(subDirectoryFull, outputLowResFile)):
+        print(outputLowResFile + " already exists. Skipping...")
+        continue
+      else:
+        print("Creating " + outputLowResFile + " from " + inputHighResFile)
+
+      bashCommand = "ffmpeg -i " + inputHighResFile + " -vf scale=" \
                                       + str(downSampleResolution[0]) + ":" \
                                       + str(downSampleResolution[1])  + " " \
                                       + outputLowResFile
