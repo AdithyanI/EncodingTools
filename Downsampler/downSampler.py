4576a1afe0a8c50b201857848dfa3caab29bca70
Adithyan <adithyan.i4internet@gmail.com>
2017-12-26 11:18:33 +0100

    Create in subdirectories instead of the same directories.

--- Downsampler/downSampler.py
+++ Downsampler/downSampler.py
@@ -6,7 +6,7 @@ def find_highres_source_file(subDirectoryFull):
    # https://stackoverflow.com/questions/3964681/find-all-files-in-a-directory-with-extension-txt-in-python     
     for file in os.listdir(subDirectoryFull):
       if file.endswith(".y4m") and file.find("1080p")!=-1:
-        print("Found source file = " + file)
+        print "Found source file = ", file
         return file
 
 def down_sampler():
@@ -20,27 +20,33 @@ def down_sampler():
                           ]
   
   for subDirectory in subDirectories:
-    subDirectoryFull = cwd + "/" + subDirectory
-    inputHighResFile = find_highres_source_file(subDirectoryFull)
+    inputHighResDirectory = os.path.join(cwd,subDirectory,"1080p")
+    inputHighResFile = find_highres_source_file(inputHighResDirectory)
 
     for downSampleResolution in downSampleResolutions:
       outputLowResFile = inputHighResFile.replace("1080p", \
                                       str(downSampleResolution[1])+"p")
+      outputLowResDirectory = os.path.join(cwd,subDirectory,(str(downSampleResolution[1])+"p"))
 
-      if os.path.exists(os.path.join(subDirectoryFull, outputLowResFile)):
-        print(outputLowResFile + " already exists. Skipping...")
+      if os.path.exists(os.path.join(outputLowResDirectory, outputLowResFile)):
+        print outputLowResFile,"already exists. Skipping..."
         continue
       else:
-        print("Creating " + outputLowResFile + " from " + inputHighResFile)
+        if not os.path.exists(outputLowResDirectory):
+          os.makedirs(outputLowResDirectory)
+        print "Creating",outputLowResFile,"from",inputHighResFile
 
-      bashCommand = "ffmpeg -i " + inputHighResFile + " -vf scale=" \
-                                      + str(downSampleResolution[0]) + ":" \
-                                      + str(downSampleResolution[1])  + " " \
-                                      + outputLowResFile
-      p1 = subprocess.Popen(bashCommand, shell=True, cwd=subDirectoryFull)
+      bashCommand = "ffmpeg -i " + os.path.join(inputHighResDirectory,inputHighResFile) + " -vf scale=" \
+                    + str(downSampleResolution[0]) + ":" + str(downSampleResolution[1])  + " " \
+                    + os.path.join(outputLowResDirectory,outputLowResFile)
+
+      p1 = subprocess.Popen(bashCommand, shell=True)
       p1.wait()
 
 if __name__ == "__main__":
+  if(len(sys.argv) < 2):
+    print "Not enough arguments. Usuage : downsampler.py </location/of/rawVideoDirectory>"
+    sys.exit()
   print "Downsampling all videos present in subfolder of -", \
   str(sys.argv[1])
   down_sampler()
