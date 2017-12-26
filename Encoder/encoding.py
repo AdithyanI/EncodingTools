cd196beb64fa14de294fa16dfa983c35d5e1a9f8
Adithyan <adithyan.i4internet@gmail.com>
2017-12-26 13:58:26 +0100

    First version of commit for encoding.py. Can create multiple encoding with a single batch testing file.

--- Encoder/encoding.py
+++ Encoder/encoding.py
@@ -1,70 +1,68 @@
 import subprocess
 import os
 import sys
+global encodingInfoSet
 
-def find_highres_source_file(subDirectoryFull):
-   # https://stackoverflow.com/questions/3964681/find-all-files-in-a-directory-with-extension-txt-in-python     
-    for file in os.listdir(subDirectoryFull):
-      if file.endswith(".y4m") and file.find("1080p")!=-1:
-        print("Found source file = " + file)
-        return file
+encodingInfoSet = [
+                {"name":"360p","width":640,"height":360,"reprBitRates":[500] },
+                #{"name":"2160p","width":4096,"height":2160,"reprBitRate":[10000,16000,28000] },
+                  ]
+
+def find_highres_source_file(directory):
+  # https://stackoverflow.com/questions/3964681/find-all-files-in-a-directory-with-extension-txt-in-python
+  for file in os.listdir(directory):
+    if file.endswith(".y4m"):
+      print("Found source file = " + file)
+      return file
+
+def create_directory(directory):
+  if not os.path.exists(directory):
+    os.makedirs(directory)
+
+def cleanup_directory(directory):
+  for item in os.listdir(directory):
+    if item.endswith(".webm") or item.endswith(".bin"):
+      os.remove(os.path.join(directory,item))
+
+def encode_all_bitrates(subDirectoryFull,video):
+  global encodingInfoSet
+  outputVideosDirectory = str(sys.argv[2])
+  aomEncoder = str(sys.argv[3])
+
+  for encodingInfo in encodingInfoSet:
+    resolutionDirectory = os.path.join(subDirectoryFull, encodingInfo["name"])
+    inputFileName = find_highres_source_file(resolutionDirectory)
+    inputFile = os.path.join(resolutionDirectory, inputFileName)
+
+    for reprBitRate in encodingInfo["reprBitRates"]:
+      print "Encoding",video,"-",encodingInfo["name"],"-",reprBitRate,"kpbs"
+
+      outputFileName = inputFileName.replace("y4m","webm")
+      outputDirectory = os.path.join(outputVideosDirectory,video,encodingInfo["name"],str(reprBitRate))
+      outputFile = os.path.join(outputDirectory, outputFileName)
+      create_directory(outputDirectory)
+      cleanup_directory(outputDirectory)
+      bashCommand = aomEncoder + " -v --good --limit=1 --target-bitrate="+str(reprBitRate)\
+                    +" -o " + outputFile + " " + inputFile
+      print(bashCommand)
+      p1 = subprocess.Popen(bashCommand, shell=True, cwd=outputDirectory)
+      p1.wait()
 
 def encoding():
   #https://stackoverflow.com/questions/973473/getting-a-list-of-all-subdirectories-in-the-current-directory
-  #cwd = str(sys.argv[1])
-  cwd = "/home/adithyan/Innovation/RawVideo"
-  subDirectories = next(os.walk(cwd))[1]
+  inputVideosDirectory = str(sys.argv[1])
+  subDirectories = next(os.walk(inputVideosDirectory))[1]
   print "Found subfolders :", subDirectories
-  encodingInfoSet = [
-                  {"name":"360p","width":640,"height":360,"reprBitRates":[500,800,1400] },
-                  {"name":"720p","width":1280,"height":720,"reprBitRates":[1500,2400,4200] },
-                  {"name":"1080p","width":1920,"height":1080,"reprBitRates":[3000,4800,8400] },
-                  #{"name":"2160p","width":4096,"height":2160,"reprBitRate":[10000,16000,28000] },
-                    ]
-  
-  for subDirectory in subDirectories:
-    subDirectoryFull = cwd + "/" + subDirectory
-    for encodingInfo in encodingInfoSet:
-      for reprBitRate in encodingInfo["reprBitRates"]:
-        print "Encoding",subDirectory,"-",encodingInfo["name"],"-",reprBitRate,"kpbs"
-        bashCommand = "./aomenc -v --good --limit=1 --target-bitrate=",reprBitRate,"-o output.webm blue_sky_1080p25.y4m"
-        p1 = subprocess.Popen(bashCommand, shell=True, cwd=subDirectoryFull)
-        p1.wait()
-
-
-
-
-    '''
-    inputHighResFile = find_highres_source_file(subDirectoryFull)
-
-
-
-
-
-    for downSampleResolution in downSampleResolutions:
-      outputLowResFile = inputHighResFile.replace("1080p", \
-                                      str(downSampleResolution[1])+"p")
-
-      if os.path.exists(os.path.join(subDirectoryFull, outputLowResFile)):
-        print(outputLowResFile + " already exists. Skipping...")
-        continue
-      else:
-        print("Creating " + outputLowResFile + " from " + inputHighResFile)
-
-      bashCommand = "ffmpeg -i " + inputHighResFile + " -vf scale=" \
-                                      + str(downSampleResolution[0]) + ":" \
-                                      + str(downSampleResolution[1])  + " " \
-                                      + outputLowResFile
-      p1 = subprocess.Popen(bashCommand, shell=True, cwd=subDirectoryFull)
-      p1.wait()
-      '''
+  for video in subDirectories:
+    subDirectoryFull = os.path.join(inputVideosDirectory,video)
+    encode_all_bitrates(subDirectoryFull, video)
 
 if __name__ == "__main__":
-  
-  #if(len(sys.argv) < 3):
-  #  print "Not enough arguments. Usuage : encoding.py </location/of/rawVideoDirectory>"
-  #  sys.exit()
+  if(len(sys.argv) < 4):
+    print "Not enough arguments. Usuage : encoding.py </location/of/inputVideo> \
+           </location/of/outputVideo> <encoder>"
+    print "Exiting!..."
+    sys.exit()
 
-  #print "Downsampling all videos present in subfolder of -", \
-  #str(sys.argv[1])
+  print "Starting encoding of videos present in folder of -", str(sys.argv[1])
   encoding()
