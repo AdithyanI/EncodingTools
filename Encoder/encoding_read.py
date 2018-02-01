e5b6b7dc36137e39a38260210b3a4fd82c340a6e
Adithyan <adithyan.i4internet@gmail.com>
2018-02-01 17:44:59 +0100

    Add encoding_read.py to properly get the content.

--- Encoder/encoding_read.py
+++ Encoder/encoding_read.py
@@ -3,13 +3,14 @@ import subprocess
 import os
 import sys
 global encodingInfoSet
+from shutil import copyfile
 
-#python encoding.py /home/adithyan/Innovation/RawVideo/ /home/adithyan/Innovation/MultiRate/PartitionReuse/ /home/adithyan/Innovation/aomenc
+#python encoding_read.py /home/adithyan/Innovation/RawVideo/ /home/adithyan/Innovation/MultiRate/PartitionReuse/Modified/ /home/adithyan/Innovation/aomenc_read
 
 encodingInfoSet = [
-                {"name":"360p","width":640,"height":360,"reprBitRates":[500,650,800,1100] },
-                {"name":"720p","width":1280,"height":720,"reprBitRates":[1500,1950,2400,3300] },
-                {"name":"1080p","width":1920,"height":1080,"reprBitRates":[3000,3900,4800,6600] }
+                {"name":"360p","width":640,"height":360,"reprBitRates":[500,650,800,1100,1400] },
+                #{"name":"720p","width":1280,"height":720,"reprBitRates":[1500,1950,2400,3300,4200] },
+                #{"name":"1080p","width":1920,"height":1080,"reprBitRates":[3000,3900,4800,6600,8400] }
                 #{"name":"2160p","width":4096,"height":2160,"reprBitRate":[10000,16000,28000] },
                   ]
 processes = set()
@@ -41,15 +42,26 @@ def encode_all_bitrates(subDirectoryFull,video):
     resolutionDirectory = os.path.join(subDirectoryFull, encodingInfo["name"])
     inputFileName = find_highres_source_file(resolutionDirectory)
     inputFile = os.path.join(resolutionDirectory, inputFileName)
+    refReprBitRate = encodingInfo["reprBitRates"][-1]
 
-    for reprBitRate in encodingInfo["reprBitRates"]:
+    #Include all bitrates except the last one.
+    for reprBitRate in encodingInfo["reprBitRates"][0:-1]:
       print "Encoding",video,"-",encodingInfo["name"],"-",reprBitRate,"kpbs"
-
       outputFileName = inputFileName.replace("y4m","webm")
       outputDirectory = os.path.join(outputVideosDirectory,video,encodingInfo["name"],str(reprBitRate))
+      referenceOutputDirectory = os.path.join(outputVideosDirectory,video,encodingInfo["name"],str(refReprBitRate))
+
+      if not os.path.exists(referenceOutputDirectory):
+        print "Error. No Reference directory found in", referenceOutputDirectory,". Breaking!"
+        break
+
+      print "Copying from",referenceOutputDirectory,"to",outputDirectory
+      copyfile(os.path.join(referenceOutputDirectory,"analysisData.bin"), os.path.join(outputDirectory,"analysisData.bin"))
+
       outputFile = os.path.join(outputDirectory, outputFileName)
       create_directory(outputDirectory)
       cleanup_directory(outputDirectory)
+
       bashCommand = aomEncoder + " --psnr --good --limit=125 --kf-max-dist=25 --kf-min-dist=25 --passes=1 --target-bitrate="+str(reprBitRate)\
                     +" -o " + outputFile + " " + inputFile + " 2> log.txt"
 
